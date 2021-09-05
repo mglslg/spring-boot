fbd15dd06924cd7448353b1a428464bd66ff1241
suolongga <mglslg@qq.com>
2021-09-05 21:01:27 +0800

    自己做个demo

--- spring-boot-project/slg-spring-demo/src/main/java/com/slg/spring/demo/SlgApplication.java
+++ spring-boot-project/slg-spring-demo/src/main/java/com/slg/spring/demo/SlgApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.slg.huli")
 public class SlgApplication {
 	public static void main(String[] args) {
+		System.out.println("lalala");
 		SpringApplication.run(SlgApplication.class, args);
 	}
 }
